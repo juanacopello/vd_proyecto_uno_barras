56253a69bd166a0338b085428b7d7468d9f4dd84
Juana Copello <copellojuana@gmail.com>
2024-09-05 17:39:55 -0300

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,88 @@
+<script>
+  /* Importamos d3 para tenerlo disponible */
+  import * as d3 from "d3"
+
+  let data = [1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000]
+
+  let altura = d3
+    .scaleLinear()
+    .domain([0, d3.max(data)])
+    .range([0, 400])
+
+  let colorLightness = d3
+    .scaleLinear()
+    .domain([0, d3.max(data)])
+    .range(["white", "green"])
+</script>
+
+<!-- Estructura contenido HTML -->
+<h2>Ejemplo 1: Altura</h2>
+<div class="container">
+  <!-- <div class="item_wrapper">
+    <p class="num">1000</p>
+    <div class="column"></div> 
+    </div>-->
+  {#each data as num}
+    <div class="item_wrapper">
+      <p class="num">{num}</p>
+      <div class="column" style="height: {altura(num)}px; background-color: {colorLightness(num)}"></div>
+    </div>
+  {/each}
+  <!-- </div> -->
+</div>
+<br />
+<br />
+
+<!-- <h2>Ejemplo 2: Color</h2>
+<div class="container_2">
+  {#each data as num}
+    <div class="item_wrapper">
+      <div class="circle" style="background-color: {colorLightness(num)}"></div>
+      <p class="num_2">{num}</p>
+    </div>
+  {/each}
+</div> -->
+
+<!-- Estilos CSS -->
+<style>
+  /* Estilos CSS */
+  .container,
+  .container_2 {
+    display: flex;
+    justify-content: space-between;
+    gap: 10px;
+    height: 450px;
+  }
+  .container_2 {
+    height: auto;
+  }
+
+  .item_wrapper {
+    display: flex;
+    flex-direction: column;
+    justify-content: end;
+    align-items: center;
+    gap: 10px;
+  }
+  .column {
+    width: 80px;
+    height: 100%;
+  }
+  .num {
+    font-size: 21px;
+    font-weight: bold;
+    margin: 0;
+    color: #ee0001;
+  }
+  .num_2 {
+    font-size: 18px;
+    margin: 0;
+    color: #ee0001;
+  }
+  .circle {
+    width: 80px;
+    height: 80px;
+    border-radius: 50%;
+    background-color: #ee0001;
+  }
+</style>
